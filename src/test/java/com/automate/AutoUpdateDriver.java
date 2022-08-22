a6e01608699b4eb8da22f7c28f77c1a9a060ab45
Honey Jain <jainhoney92@gmail.com>
2022-08-22 12:13:03 +0530

    Updated POM & Driver File

--- src/test/java/com/automate/AutoUpdateDriver.java
+++ src/test/java/com/automate/AutoUpdateDriver.java
@@ -2,6 +2,8 @@ package com.automate;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,20 +11,27 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class AutoUpdateDriver {
 	WebDriver driver;
 
-	@Test(priority=3)
-	public void test1() {
+	@Parameters("Browser")
+	@Test(priority = 3)
+	public void startBrowser(String browserName) {
+		System.out.println("Parameter value is " + browserName);
 		AutoUpdateDriver obj = new AutoUpdateDriver();
-		driver = obj.getChromeDriver();
+		if (browserName.equalsIgnoreCase("Chrome")) {
+			driver = obj.getChromeDriver();
+		}
+		if (browserName.equalsIgnoreCase("Firefox")) {
+			driver = obj.getFireFoxDriver();
+		}
 		driver.get("https://www.google.co.in/");
-
+		driver.quit();
 	}
 
-	@Test(priority=1)
+	@Test(priority = 1)
 	public void test2() {
 		System.out.println("Test second");
 	}
 
-	@Test(priority=2)
+	@Test(priority = 2)
 	public void test3() {
 		System.out.println("Test First");
 	}
@@ -32,6 +41,13 @@ public class AutoUpdateDriver {
 		// "chromedriver.exe");
 		WebDriverManager.chromedriver().setup();
 		return new ChromeDriver();
+	}
+
+	private static WebDriver getFireFoxDriver() { //
+		// System.setProperty("webdriver.chrome.driver", DRIVER_DIR +
+		// "chromedriver.exe");
+		WebDriverManager.firefoxdriver().setup();
+		return new FirefoxDriver();
 	}
 
 }
